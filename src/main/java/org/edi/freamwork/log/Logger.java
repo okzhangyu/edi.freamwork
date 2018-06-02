253fba14d06b0901fc90eadc67a86f98988b3cb0
fancys <fancy553973204@gmail.com>
2018-06-02 12:01:26 +0800

    初始化基础框架

---
+++ src/main/java/org/edi/freamwork/log/Logger.java
@@ -0,0 +1,8 @@
+package org.edi.freamwork.log;
+
+/**
+ * @author Fancy
+ * @date 2018/6/2
+ */
+public class Logger {
+}
---
+++ src/test/java/TestLogger.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+
+/**
+ * @author Fancy
+ * @date 2018/6/2
+ */
+public class TestLogger extends TestCase {
+
+}
