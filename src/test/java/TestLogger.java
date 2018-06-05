5161c080ceaeafdcf72182f2bfbbfe4c76db0194
fancys <fancy553973204@gmail.com>
2018-06-05 17:59:18 +0800

    添加日志封装

--- src/test/java/TestLogger.java
+++ src/test/java/TestLogger.java
@@ -1,5 +1,4 @@
 import junit.framework.TestCase;
-import org.edi.freamwork.log.APPLogger;
 import org.edi.freamwork.log.LogFileName;
 import org.edi.freamwork.log.LoggerUtils;
 import org.junit.Test;
@@ -12,22 +11,21 @@ import org.slf4j.Logger;
 public class TestLogger extends TestCase {
 
     //https://blog.csdn.net/mggwct/article/details/77718122
-    @Test
-    public void testELogger(){
-        APPLogger appLogger = new APPLogger();
-        appLogger.ELogger("this is test");
-    }
+
 
     @Test
     public void testDifPathLog(){
         Logger ysyjLogger = LoggerUtils.Logger(LogFileName.YSYJ);
         Logger avaLogger = LoggerUtils.Logger(LogFileName.AVA);
-
+        Logger edfLogger = LoggerUtils.Logger("edf");
         ysyjLogger.info("YSYJ's log");
         avaLogger.info("AVA's log");
 
         ysyjLogger.error("YSYJ's error log");
         avaLogger.error("AVA's error log");
+
+        edfLogger.info("EDF's log");
+        edfLogger.error("EDF's error");
     }
 
 }
