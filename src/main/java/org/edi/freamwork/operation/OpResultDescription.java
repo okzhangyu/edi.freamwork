6cb37a121fd48f69c6125872101baf447acfaca9
fancys <fancy553973204@gmail.com>
2018-07-13 17:01:51 +0800

    添加操作结果常量类

---
+++ src/main/java/org/edi/freamwork/operation/OpResultDescription.java
@@ -0,0 +1,16 @@
+package org.edi.freamwork.operation;
+
+/**
+ * @author Fancy
+ * @date 2018/7/13
+ * 操作结果描述
+ */
+public class OpResultDescription {
+
+    public static final String OP_SUCCESSFUL="operation is successful";
+
+    public static final String VALUE_IS_EMPTY= "[%s]'s value is empty";
+
+    public static final String DATA_IS_EMPTY="data is empty";
+
+}
