bada7f9d66c0e889f17fbb2541d2625324c88aa9
fancys <fancy553973204@gmail.com>
2018-07-31 19:48:15 +0800

    调整服务

--- src/main/java/org/edi/freamwork/exception/BusinessException.java
+++ src/main/java/org/edi/freamwork/exception/BusinessException.java
@@ -7,8 +7,13 @@ package org.edi.freamwork.exception;
 
 public class BusinessException extends RuntimeException {
 
-
     private static final long serialVersionUID = 1L;
+
+    private String code = "";
+
+    public String getCode(){
+        return code;
+    }
 
     public BusinessException() {
     }
@@ -27,5 +32,10 @@ public class BusinessException extends RuntimeException {
 
     public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public BusinessException(String code,String message){
+        super(message);
+        this.code = code;
     }
 }
