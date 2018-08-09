69722009884a2423444533621abc80762d173212
fancys <fancy553973204@gmail.com>
2018-08-09 18:32:13 +0800

    B1单据生成修正

--- src/main/java/org/edi/freamwork/data/DataConvertException.java
+++ src/main/java/org/edi/freamwork/data/DataConvertException.java
@@ -5,11 +5,20 @@ package org.edi.freamwork.data;
  * @date 2018/7/19
  */
 public class DataConvertException extends RuntimeException{
+
+    private String code;
+    private String message;
     public DataConvertException() {
     }
 
     public DataConvertException(String message) {
         super(message);
+        this.message = message;
+    }
+    public DataConvertException(String code,String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
     }
 
     public DataConvertException(Throwable cause) {
@@ -18,10 +27,20 @@ public class DataConvertException extends RuntimeException{
 
     public DataConvertException(String message, Throwable cause) {
         super(message, cause);
+        this.message = message;
     }
 
     public DataConvertException(String message, Throwable cause, boolean enableSuppression,
                                 boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+        this.message = message;
+    }
+
+    public String getCode(){
+        return code;
+    }
+    @Override
+    public String getMessage(){
+        return message;
     }
 }
